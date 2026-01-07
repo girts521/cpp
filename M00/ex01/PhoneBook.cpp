7befe1816cc6d85393a6bb7b7a9214680abe302b
Girts Karcevskis <gikarcev@c4a10c10.42berlin.de>
2026-01-07 18:06:49 +0100

    removed an unused var

--- M00/ex01/PhoneBook.cpp
+++ M00/ex01/PhoneBook.cpp
@@ -1,35 +1,31 @@
 #include "PhoneBook.hpp"
 #include "Contact.hpp"
 #include "utils.hpp"
+#include <cstdlib>
 #include <iostream>
 #include <string>
-#include <iomanip>
-#include <cstdlib>
 
-PhoneBook::PhoneBook(void)
-{
+PhoneBook::PhoneBook(void) {
   std::cout << "The default constructor has been called" << std::endl;
   this->_index = 0;
 }
 
-PhoneBook::PhoneBook(const PhoneBook &src) : _index(src._index)
-{
+PhoneBook::PhoneBook(const PhoneBook &src) : _index(src._index) {
   std::cout << "The copy constructor has been called" << std::endl;
 
   for (int i = 0; i < 8; i++)
     this->_contact_list[i] = src._contact_list[i];
 }
 
-PhoneBook::~PhoneBook(void)
-{
+PhoneBook::~PhoneBook(void) {
   std::cout << "The destructor has been called" << std::endl;
 }
 
-PhoneBook &PhoneBook::operator=(const PhoneBook &src)
-{
+PhoneBook &PhoneBook::operator=(const PhoneBook &src) {
   std::cout << "The assignment operator has been called" << std::endl;
 
-  if (this == &src) return *this;
+  if (this == &src)
+    return *this;
 
   this->_index = src._index;
   for (int i = 0; i < 8; i++)
@@ -38,8 +34,7 @@ PhoneBook &PhoneBook::operator=(const PhoneBook &src)
   return *this;
 }
 
-void PhoneBook::addContact(void)
-{
+void PhoneBook::addContact(void) {
   Contact new_contact;
 
   std::cout << "Adding a new contact" << std::endl;
@@ -50,36 +45,37 @@ void PhoneBook::addContact(void)
   Utils::setVar(new_contact.phone_number, "Please enter your phone number: ");
   new_contact.setDarkestSecret();
 
-  if (_index == 8) _index = 0;
+  if (_index == 8)
+    _index = 0;
   _contact_list[_index++] = new_contact;
 }
 
-void PhoneBook::searchContact(void)
-{
+void PhoneBook::searchContact(void) {
   int index;
   std::string s_index;
 
-  if (this->_index == 0) 
-  {
-      std::cout << "Phonebook is empty!" << std::endl;
-      return;
+  if (this->_index == 0) {
+    std::cout << "Phonebook is empty!" << std::endl;
+    return;
   }
 
   Utils::printTable(_contact_list, 8);
   std::cout << "Index: ";
-  if (!std::getline(std::cin, s_index)) return;
+  if (!std::getline(std::cin, s_index))
+    return;
   index = std::atoi(s_index.c_str());
 
-  while (index <= 0 || index > 8 || _contact_list[index - 1].name.empty())
-  {
+  while (index <= 0 || index > 8 || _contact_list[index - 1].name.empty()) {
     std::cout << "Enter a correct Index: ";
-    if (!std::getline(std::cin, s_index)) return;
+    if (!std::getline(std::cin, s_index))
+      return;
     index = std::atoi(s_index.c_str());
   }
 
   std::cout << "First name: " << _contact_list[index - 1].name << std::endl;
   std::cout << "Last name: " << _contact_list[index - 1].last_name << std::endl;
   std::cout << "Nickname: " << _contact_list[index - 1].nickname << std::endl;
-  std::cout << "Phone number: " << _contact_list[index - 1].phone_number << std::endl;
+  std::cout << "Phone number: " << _contact_list[index - 1].phone_number
+            << std::endl;
   _contact_list[index - 1].printSecret();
 }
